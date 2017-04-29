e95a77d1cb5b8a5ed5c3587179a96069b7834ed8
Gerard Abelló Serras <gerardabello@gmail.com>
2017-04-29 13:01:59 +0200

    Prepare compare for zoom

--- src/ImageCompare.js
+++ src/ImageCompare.js
@@ -5,15 +5,17 @@ import $ from './ImageCompare.css'
 
 class ImageCompare extends Component {
   render () {
+    let transformStyle = 'scale(0.9)'
+
     return (
       <div className={$.imageCompare}>
         <div className={$.imageWrapper}>
-          <div className={$.image} style={{backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.baseimage})`}}/>
-          <div className={cx($.image, $.diff)} style={{backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
+          <div className={$.image} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.baseimage})`}}/>
+          <div className={cx($.image, $.diff)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
         </div>
         <div className={$.imageWrapper}>
-          <div className={$.image} style={{backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.image})`}}/>
-          <div className={cx($.image, $.diff)} style={{backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
+          <div className={$.image} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.image})`}}/>
+          <div className={cx($.image, $.diff)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
         </div>
       </div>
 
