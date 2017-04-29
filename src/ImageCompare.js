0a94238f1d2f550d2c7f09a9cff38dff98f1d2d5
Gerard Abelló Serras <gerardabello@gmail.com>
2017-04-29 18:27:13 +0200

    Add image dragging

--- src/ImageCompare.js
+++ src/ImageCompare.js
@@ -7,26 +7,59 @@ class ImageCompare extends Component {
 	constructor () {
 		super()
 
-		this.state = { scale: 0.9, translate: {x: 0, y: 0} }
+		this.state = { dragging: false, scale: 0.9, translate: {x: 0, y: 0} }
+
+		this.handleMouseDown = this.handleMouseDown.bind(this)
+		this.handleMouseUp = this.handleMouseUp.bind(this)
+		this.handleMouseMove = this.handleMouseMove.bind(this)
 	}
 
-  render () {
-    let transformStyle = `scale(${this.state.scale}) translateX(${this.state.translate.x}px) translateY(${this.state.translate.y}px)`
+	handleMouseDown () {
+		this.setState({ dragging: true })
+	}
 
-    return (
-      <div className={$.imageCompare}>
-        <div className={$.imageWrapper}>
-          <div className={$.image} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.baseimage})`}}/>
-          <div className={cx($.image, $.diff)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
-        </div>
-        <div className={$.imageWrapper}>
-          <div className={$.image} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.image})`}}/>
-          <div className={cx($.image, $.diff)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
-        </div>
-      </div>
+	handleMouseUp () {
+		this.setState({ dragging: false })
+	}
 
-    )
-  }
+	handleMouseMove (e) {
+		if (this.state.dragging) {
+			this.setState({
+				translate: {
+					x: this.state.translate.x + e.screenX - this.lastMouseX,
+					y: this.state.translate.y + e.screenY - this.lastMouseY,
+				}
+			})
+		}
+
+		this.lastMouseX = e.screenX
+		this.lastMouseY = e.screenY
+	}
+
+	render () {
+		let transformStyle = `scale(${this.state.scale}) translateX(${this.state.translate.x}px) translateY(${this.state.translate.y}px)`
+
+		let smoothTransform = { [$.smoothTransform]: !this.state.dragging }
+
+		return (
+			<div
+				className={$.imageCompare}
+				onMouseDown={this.handleMouseDown}
+				onMouseUp={this.handleMouseUp}
+				onMouseMove={this.handleMouseMove}
+			>
+				<div className={$.imageWrapper}>
+					<div className={cx($.image, smoothTransform)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.baseimage})`}}/>
+					<div className={cx($.image, $.diff, smoothTransform)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
+				</div>
+				<div className={$.imageWrapper}>
+					<div className={cx($.image, smoothTransform)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.image})`}}/>
+					<div className={cx($.image, $.diff, smoothTransform)} style={{transform: transformStyle, backgroundImage: `url(${process.env.OPTICIAN_API_URL}/image/${this.props.diffimage})`}}/>
+				</div>
+			</div>
+
+		)
+	}
 }
 
 export default ImageCompare
